b2383fb071a4d01b39a1a8d63c8c6a49bb65ed24
faOC20 <fabianortizfc@gmail.com>
2025-04-04 21:28:21 -0400

    array for technologies of toolset section

---
+++ src/constants/technologies.ts
@@ -0,0 +1,121 @@
+import Turso from '@icons/technologies/turso.astro';
+import Firebase from '@icons/technologies/firebase.astro';
+import MongoDB from '@icons/technologies/mongodb.astro';
+import React from '@icons/technologies/react.astro';
+import TypeScript from '@icons/technologies/typescript.astro';
+import Node from '@icons/technologies/node.astro';
+import MySQL from '@icons/technologies/mysql.astro';
+import SQLite from '@icons/technologies/sqlite.astro';
+import Python from '@icons/technologies/python.astro';
+import Go from '@icons/technologies/go.astro';
+import Tailwind from '@icons/technologies/tailwind.astro';
+import Astro from '@icons/technologies/astro.astro';
+import Html from '@icons/technologies/html.astro';
+import Css from '@icons/technologies/css.astro';
+import Express from '@icons/technologies/express.astro';
+
+interface Technology {
+    category: string;
+    techs: {
+        name: string;
+        icon: any;
+        category: string;
+    }[];
+}
+
+
+export const TECHNOLOGIES: Technology[] = [
+    {
+        category: 'frontend',
+        techs: [
+            {
+                name: 'React',
+                icon: React,
+                category: 'Frontend'
+            },
+            {
+                name: 'TypeScript',
+                icon:  TypeScript,
+                category: 'Frontend'
+            },
+            {
+                name: 'Tailwind',
+                icon: Tailwind,
+                category: 'Frontend'
+            },
+            {
+                name: 'Astro',
+                icon: Astro,
+                category: 'Frontend'
+            },
+            {
+                name: 'HTML',
+                icon: Html,
+                category: 'Frontend'
+            },
+            {
+                name: 'CSS',
+                icon: Css,
+                category: 'Frontend'
+            },
+            
+        ]
+    },
+    {
+        category: 'backend',
+        techs: [
+            {
+                name: 'Node',
+                icon: Node,
+                category: 'Backend'
+            },
+            {
+                name: 'Python',
+                icon: Python,
+                category: 'Backend'
+            },
+            {
+                name: 'Go',
+                icon: Go,
+                category: 'Backend'
+            },
+            {
+                name: 'Express',
+                icon: Express,
+                category: 'Backend'
+            },
+            
+        ]
+    },
+    {
+        category: 'services',
+        techs: [
+            {
+                name: 'Firebase',
+                icon: Firebase,
+                category: 'Services'
+            },
+            {
+                name: 'MongoDB',
+                icon: MongoDB,
+                category: 'Services'
+            },
+            {
+                name: 'MySQL',
+                icon: MySQL,
+                category: 'Services'
+            },
+            {
+                name: 'SQLite',
+                icon: SQLite,
+                category: 'Services'
+            },
+            {
+                name: 'Turso',
+                icon: Turso,
+                category: 'Services'
+            },
+        ]
+    }
+
+] as const
